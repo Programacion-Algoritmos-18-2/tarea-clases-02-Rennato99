df2815605f5883d5e1b52267962629a2cdbf0b78
Renato Balcazar <renatojbl99@gmail.com>
2018-10-12 12:11:53 -0500

    Subiendo todo el ejercicio comentado

--- Ejercicio_Final/EjercicoFinal/src/ejercicofinal/Equipo.java
+++ Ejercicio_Final/EjercicoFinal/src/ejercicofinal/Equipo.java
@@ -5,37 +5,39 @@ package ejercicofinal;
  * @author Renato
  */
 public class Equipo {
-    String nombre;
-    int num_jugadores;
-    String ciudad;
+    
+    // Atributos
+    String nombre;      // Valor por defecto del nmbre del equipo = ""
+    int num_jugadores;  // Valor por defecto del numero de jugadores = 0
+    String ciudad;      // Valor por defecto de la ciudad del equipo = ""
     
     // Constructor 1
     public Equipo(){
-        this.nombre = "SinNombre";
-        this.num_jugadores = 15;
-        this.ciudad = "SinCiudad";    
+        this.nombre = "SinNombre";      // Valor por defecto del nombre del equipo = "SinNombre"
+        this.num_jugadores = 15;        // Valor por defecto de numero de jugadores = 15
+        this.ciudad = "SinCiudad";      // Valor por defecto de la ciudad del equipo = "SinCiudad"
     }
     
-    // Constructor 2
+    // Constructor 2    
     public Equipo(String nombre){
-        this.nombre = nombre;
+        this.nombre = nombre;               // (Recibe de parámetro el nombre del equipo)
     }
     
-    // Constructor 3
+    // Constructor 3  
     public Equipo(String nombre, int jugadores){
-        this.nombre = nombre;
-        this.num_jugadores = jugadores;
+        this.nombre = nombre;                // (Recibe de parámetro el nombre del equipo)
+        this.num_jugadores = jugadores;      // (Recibe de parámetro el nombre del equipo)
     }
     
     // Constructor 4
     public Equipo(String nombre, int jugadores, String nombreCiudad){
-        this.nombre = nombre;
-        this.num_jugadores = jugadores;
-        this.ciudad = nombreCiudad;
+        this.nombre = nombre;               // (Recibe de parámetro el nombre del equipo)
+        this.num_jugadores = jugadores;     // (Recibe de parámetro el nombre del equipo)  
+        this.ciudad = nombreCiudad;         // (Recibe de parámetro el nombre de la ciudad)
     }
     
     // Métodos GET nombre
-    public String obtener_nombre(){
+    public String obtener_nombre(){         
         return this.nombre;
     }
     
@@ -51,13 +53,13 @@ public class Equipo {
     
     // Método SET de num_jugadores
     public void agregar_num_jugadores(int jugadores) {
-        this.num_jugadores = jugadores;
+        this.num_jugadores = jugadores;                 // Agrega valor a la variable global num_jugadores
     }
     
     // Método SET de ciudad
     public void agregar_ciudad(String nombreCiudad){
-        this.ciudad = nombreCiudad;
+        this.ciudad = nombreCiudad;                     // Agrega valor a la variable global ciudad
     }
  
-    
+   
 }
--- Ejercicio_Final/EjercicoFinal/src/ejercicofinal/Principal.java
+++ Ejercicio_Final/EjercicoFinal/src/ejercicofinal/Principal.java
@@ -7,23 +7,23 @@ package ejercicofinal;
 public class Principal {
     public static void main(String[] args) {
         // Objeto #1
-        Equipo e = new Equipo();
-        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e.obtener_nombre(), e.obtener_num_jugadores(), e.obtener_ciudad());
+        Equipo e = new Equipo();   // Utilizamos el Constructor #1
+        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e.obtener_nombre(), e.obtener_num_jugadores(), e.obtener_ciudad()); // Presentar datos
         
         // Objeto #2
-        Equipo e2 = new Equipo("Liga de Loja");
+        Equipo e2 = new Equipo("Liga de Loja"); // Utilizamos el Constructor #2
         e2.agregar_num_jugadores(20);
         e2.agregar_ciudad("Loja");
-        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e2.obtener_nombre(), e2.obtener_num_jugadores(), e2.obtener_ciudad());
+        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e2.obtener_nombre(), e2.obtener_num_jugadores(), e2.obtener_ciudad()); // Presentar datos
         
         // Objeto #3
-        Equipo e3 = new Equipo("Liga de Quito", 25);
+        Equipo e3 = new Equipo("Liga de Quito", 25); // Utilizamos el Constructor #3
         e3.agregar_ciudad("Quito");
-        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e3.obtener_nombre(), e3.obtener_num_jugadores(), e3.obtener_ciudad());
+        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e3.obtener_nombre(), e3.obtener_num_jugadores(), e3.obtener_ciudad()); // Presentar datos
         
         // Objeto #4
-        Equipo e4 = new Equipo("Liga de Tulcan", 30, "Tulcan");
-        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e4.obtener_nombre(), e4.obtener_num_jugadores(), e4.obtener_ciudad());
+        Equipo e4 = new Equipo("Liga de Tulcan", 30, "Tulcan"); // Utilizamos el Constructor #4
+        System.out.printf("Nombre %s - Jugadores %s - Ciudad %s\n", e4.obtener_nombre(), e4.obtener_num_jugadores(), e4.obtener_ciudad()); // Presentar datos
     
     }
 }
